7dc657f3e2c6d405f9df81bc399dae794173aa82
Phil <pk.oaferina@gmail.com>
2023-04-25 16:28:16 +0400

    filtered map for featured movies, landing page done

--- index.js
+++ index.js
@@ -2,22 +2,34 @@
 const movieWrapper = document.querySelector('.movie-list');
 
 async function main(){
-	const movies = await fetch("https://www.omdbapi.com/?apikey=f5504bbb&s=drama");
+	const movies = await fetch("https://www.omdbapi.com/?apikey=f5504bbb&s=horror");
 	const moviesData = await movies.json();
-	// console.log(moviesData.Search[0]);
+	console.log(moviesData.Search)
 
-	movieWrapper.innerHTML = moviesData.Search.map((movie) => {
-		return `
-			<div class="movie">
+	if(document.URL.includes("index.html")) {
+		return movieWrapper.innerHTML = moviesData.Search.slice(0,4).map((movie) => movieHTML(movie)).join("");
+	}
+
+}
+
+function movieHTML(movie) {
+	return `
+	<div class="movie">
 				<figure class="movie__img--wrapper">
-					<img class="move__img" src="${movie.Poster}" alt="">
+					<img class="movie__img" src="${movie.Poster}" alt="">
 				</figure>
 				<div class="movie__title">
 					${movie.Title}
 				</div>
-			</div>`
-	}).join("");
+				<div class="movie__year">
+					${movie.Year}
+				</div>
+	</div>
+	`;
+}
 
+function navHTML() {
+	
 }
 
 main();
