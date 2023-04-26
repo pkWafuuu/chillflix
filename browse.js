937bbf930c079937eb566e6b5acc8ad7dce3b9a1
Phil <pk.oaferina@gmail.com>
2023-04-26 14:14:07 +0400

    error handlers added for search

--- browse.js
+++ browse.js
@@ -7,7 +7,15 @@ async function main(search){
 	const moviesData = await movies.json();
 	// console.log(moviesData.Search)
 
-movieWrapper.innerHTML = moviesData.Search.slice(0,8).map((movie) => movieHTML(movie)).join("");
+	if(search === null) {
+		movieWrapper.innerHTML = browseErrorHTML(null);
+	}
+	else if(moviesData.Response == 'False'){
+		movieWrapper.innerHTML = browseErrorHTML(moviesData.Error, search);
+	}
+	else {
+		movieWrapper.innerHTML = moviesData.Search.slice(0,8).map((movie) => movieHTML(movie)).join("");
+	}
 
 }
 
@@ -31,6 +39,29 @@ function movieHTML(movie) {
 				</div>
 	</div>
 	`;
+}
+
+function browseErrorHTML(error, search){
+	if (error === null) {
+		return `
+			<div class="browse__empty">
+				<figure class="browse__img--wrapper">
+					<img src="./assets/browse.svg" alt="" class="browse__img">
+				</figure>
+				<h1 class="browse__text">Search For Movies, ex. Title, keywords</h1>
+			</div>
+	`
+	}
+	return `
+			
+			<div class="browse__empty">
+				<figure class="browse__img--wrapper">
+					<img src="./assets/browse.svg" alt="" class="browse__img">
+				</figure>
+				<h1 class="browse__text">Search For Movies, ex. Title, keywords</h1>
+				<h1 class="browse__text">"${search}" ${error}</h1>
+			</div>
+	`
 }
 
 main(searchKey);
